38d75315d1bd98fb47d5be4c754df0b14152755e
Siva-allu <101029742+Siva-allu@users.noreply.github.com>
2025-09-22 14:47:19 +0530

    Add files via upload

---
+++ Backend/Controllers/InvoiceController.cs
@@ -0,0 +1,56 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Microsoft.Data.SqlClient;
+using System.Text.Json;
+
+namespace BuggyApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class InvoiceController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetInvoice()
+        {
+
+            // receiving  Globalization Invariant Mode is not supported in while opening connection with higher .Net versions.
+            /*    
+            List<Item> items = [];
+            using (SqlConnection invoiceDisplay = new SqlConnection("Server=(localdb)\\mssqllocaldb;Database=aspnet-53bc9b9d-9d6a-45d4-8429-2a2761773502;Trusted_Connection=True;MultipleActiveResultSets=true"))
+            {
+                invoiceDisplay.Open();
+                string query = "SELECT * from InvoiceItems";
+                using(SqlCommand  command = new SqlCommand(query,invoiceDisplay)) {
+                    using(SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while(reader.Read())
+                        {
+                            Item  item = new Item
+                            {
+                                name = reader.GetString(reader.GetOrdinal("Name")),
+                                price = reader.GetDouble(reader.GetOrdinal("Price")),
+                            };
+                            items.Add(item);
+                        }
+
+                    }
+                }
+                invoiceDisplay.Close();
+            }*/
+
+            List<Item> items = [new Item { name = "Widget A", price = 19.99 }];
+            if (items.Count != 0)
+            {
+                return Ok(new { items });
+            }
+            return NotFound("No invoice found");
+        }
+
+        public class Item
+        {
+            public string name { get; set; }
+            public double price { get; set; }
+        }
+    }
+}
---
+++ Backend/Program.cs
@@ -0,0 +1,61 @@
+
+namespace ThinkBridge
+{
+    public class Program
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(
+                    builder =>
+                    {
+                        builder.WithOrigins("http://localhost:3000", 
+                                            "null") 
+                               .AllowAnyHeader()
+                               .AllowAnyMethod();
+                    });
+            });
+        }
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            builder.Services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(
+                    builder =>
+                    {
+                        builder.WithOrigins("*",
+                                            "null")
+                               .AllowAnyHeader()
+                               .AllowAnyMethod();
+                    });
+            });
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+                app.UseCors();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+            app.UseCors("*");
+            app.Run();
+        }
+    }
+}
